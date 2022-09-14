6a6b6e58531f7433b1d77e66964c185f08be6bc3
albaddev <alioubkeita95@gmail.com>
2022-09-14 14:09:57 +0000

    New: add Navbar component ans its style module i sass

---
+++ Components/Navbar.js
@@ -0,0 +1,15 @@
+import style from '../styles/NavBar.module.scss'
+
+
+
+const Navbar = () => {
+
+
+    return (
+        <section className={style.navBrContainer}>
+            Navbar
+        </section>
+    )
+}
+
+export default Navbar
