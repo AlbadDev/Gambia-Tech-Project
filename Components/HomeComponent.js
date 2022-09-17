6d2593b511101eac0380232e5cc33e48b112790e
albaddev <alioubkeita95@gmail.com>
2022-09-17 13:49:19 +0000

    Update: transform homeRigth component p to lowercase and add hr line

--- Components/HomeComponent.js
+++ Components/HomeComponent.js
@@ -10,11 +10,13 @@ const HomeComponent = () => {
     return (
         <section className={style.homeSectionComponent}>
             <article className={style.homeRigthComponent}>
-                <h2>INCUBATION</h2>
+                <h2>Incubation</h2>
                 <h2>& </h2>
-                <h2>INNOVATION</h2>
+                <h2>Innovation</h2>
                 <p>The Gambia Tech Project is a program deployed by the French Embassy in collaboration with the French Alliance of Banjul to foster and participate actively in the development of a Tech ecosystem that will offer opportunities and facilitate innovations for the young entrepreneurs. With a dedicated incubation space and Tech Lab opened within the Alliance, the project is giving the opportunity to all entrepreneurs to have access to different resources and opportunities.</p>
+                <hr className={style.hr}/>
                 <div className={style.rigthComponentImage}>
+                    
                     <Image className={style.afLogo} src={afbanjul} width='110' height='40'/>
                     <Image className={style.afLogo} src='http://gambiatechproject.org/wp-content/uploads/2022/07/France-Embassy-logo.png' width='110' height='40'/>
                 </div>
