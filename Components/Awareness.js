e2c8c7d2c4b90c9e2f212011b77ac8920a6cc9c4
albaddev <alioubkeita95@gmail.com>
2022-09-22 14:49:51 +0000

    Update: chance awareness card background color

--- Components/Awareness.js
+++ Components/Awareness.js
@@ -9,37 +9,48 @@ const Awareness = () => {
         <section className={style.container}>
             <h2 className={style.header}><span className={style.lineHigthLigth}>We</span> Raise Awareness</h2>
             <section className={style.sectionContainer}>
+                
                 <section className={style.awareness}>
-                    <div className={style.awareImg1}></div>
-                    <div className={style.awareText}>
-                        <h3>Tech Events</h3>
-                        <p>Gambia Tech Project grow a wide list of events around Tech to raise awareness and help entrepreneurs to understand a lot more.</p>
-                    </div>
-                    <div className={style.learnMoreContainer}>
-                        <button className={style.learnMoreButton}>Learn More &#x2192;</button>
-                    </div>
+                    <section className={style.awareness1}>
+                        <div className={style.awareImg1}></div>
+                        <div className={style.awareText}>
+                            <h3>Tech Events</h3>
+                            <p>Gambia Tech Project grow a wide list of events around Tech to raise awareness and help entrepreneurs to understand a lot more.</p>
+                        </div>
+                        <div className={style.learnMoreContainer}>
+                            <button className={style.learnMoreButton}>Learn More &#x2192;</button>
+                        </div>
+                    </section>
+                    
                 </section>
 
                 <section className={style.awareness}>
-                    <div className={style.awareImg2}></div>
-                    <div className={style.awareText}>
-                        <h3>Tech Resources</h3>
-                        <p>We offer a wide range of services to support entrepreneurs and stakeholders in our Lab open to the public</p>
-                    </div> 
-                    <div className={style.learnMoreContainer}>
-                        <button className={style.learnMoreButton}>Learn More &#x2192;</button>
-                    </div>   
+                    <section className={style.awareness2}>
+                        <div className={style.awareImg2}></div>
+                        <div className={style.awareText}>
+                            <h3>Tech Resources</h3>
+                            <p>We offer a wide range of services to support entrepreneurs and stakeholders in our Lab open to the public</p>
+                        </div> 
+                        <div className={style.learnMoreContainer}>
+                            <button className={style.learnMoreButton}>Learn More &#x2192;</button>
+                        </div>
+                    </section>
+                       
                 </section> 
                 <section className={style.awareness}>
-                    <div className={style.awareImg3}></div>
-                    <div className={style.awareText}>
-                        <h3>Startups acceleration</h3>
-                        <p>We have different programs, workshops and an incubator to support, train and accelerate Tech Startups</p>
-                    </div>
-                    <div className={style.learnMoreContainer}>
-                        <button className={style.learnMoreButton}>Learn More &#x2192;</button>
-                    </div>   
+                    <section className={style.awareness3}>
+                        <div className={style.awareImg3}></div>
+                        <div className={style.awareText}>
+                            <h3>Startups acceleration</h3>
+                            <p>We have different programs, workshops and an incubator to support, train and accelerate Tech Startups</p>
+                        </div>
+                        <div className={style.learnMoreContainer}>
+                            <button className={style.learnMoreButton}>Learn More &#x2192;</button>
+                        </div> 
+                    </section>
+                      
                 </section>
+            
             </section>
         </section>
     )
